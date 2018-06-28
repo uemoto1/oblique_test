268bca77657ffcac9b75709489221aa3a71a375c
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2018-06-28 10:30:59 +0900

    fix oblique and dataframe python scripts

--- dataframe.py
+++ dataframe.py
@@ -3,33 +3,32 @@ import os
 import numpy as np
 from numpy import sin, cos, exp, pi, e, sqrt
 
+class DataFrame:
 
-class SFDTDGrid:
-
-    def __init__(self, sysname, directory=os.cwd):
-        self.file_ac_info = os.path.join(directory, "%s_ac.info" % sysname)
-        self.file_ac_bin = os.path.join(directory, "%s_ac.bin" % sysname)
-        with open(self.file_ac_info, "r") as fh_ac_info:
-            tmp_mx1, tmp_mx2, tmp_dx = fh_ac_info.readline().split()
-            tmp_my1, tmp_my2, tmp_dy = fh_ac_info.readline().split()
-            tmp_mz1, tmp_mz2, tmp_dz = fh_ac_info.readline().split()
-            tmp_mt1, tmp_mt2, tmp_dt = fh_ac_info.readline().split()
-        self.fh_ac_bin = open(self.file_ac_bin, "r")
-        self.mx1, self.mx2, self.dx = np.imt(
-            tmp_mx1), np.imt(tmp_mx2), float(tmp_dx)
-        self.my1, self.my2, self.dy = np.imt(
-            tmp_my1), np.imt(tmp_my2), float(tmp_dy)
-        self.mz1, self.mz2, self.dz = np.imt(
-            tmp_mz1), np.imt(tmp_mz2), float(tmp_dz)
-        self.mt1, self.mt2, self.dt = np.imt(
-            tmp_mt1), np.imt(tmp_mt2), float(tmp_dt)
+    def __init__(self, title, directory=os.cwd):
+        self.file_info = os.path.join(directory, '%s.info' % title)
+        with open(self.file_info, "r") as fh_info:
+            tmp_mx1, tmp_mx2, tmp_dx = fh_info.readline().split()
+            tmp_my1, tmp_my2, tmp_dy = fh_info.readline().split()
+            tmp_mz1, tmp_mz2, tmp_dz = fh_info.readline().split()
+            tmp_mt1, tmp_mt2, tmp_dt = fh_info.readline().split()
+        self.mx1, self.mx2 = np.int(tmp_mx1), np.int(tmp_mx2)
+        self.my1, self.my2 = np.int(tmp_my1), np.int(tmp_my2)
+        self.mz1, self.mz2 = np.int(tmp_mz1), np.int(tmp_mz2)
+        self.mt1, self.mt2 = np.int(tmp_mt1), np.int(tmp_mt2)
+        self.dx = float(tmp_dx)
+        self.dy = float(tmp_dy)
+        self.dz = float(tmp_dz)
+        self.dt = float(tmp_dt)
         self.mx = self.mx2 - self.mx1 + 1
         self.my = self.my2 - self.my1 + 1
         self.mz = self.mz2 - self.mz1 + 1
-        # self.mt = self.mt2 - self.mt1 + 1
+        self.mt = self.mt2 - self.mt1 + 1
+        self.file_bin = os.path.join(directory, '%s.bin' % title)
+        self.fh_bin = open(self.file_bin, "r")
 
     def close(self):
-        return self.fh_ac_bin.close()
+        return self.fh_bin.close()
 
     def _seek_position(self, ix, iy, iz, it, check_range):
         if check_range:
@@ -41,37 +40,37 @@ class SFDTDGrid:
                 raise IndexError("z-index out of range")
             if (it < self.mt1 or self.mt2 < it):
                 raise IndexError("time-index out of range")
-        self.fh_ac_bin.seek(
+        self.fh_bin.seek(
             8 * 3 * (ix + self.mx * (iy + self.my * (iz + self.mz * it)))
         )
 
-    def read_xyz(self, it, check_range=True):
+    def read_field_on_xyz(self, it, check_range=True):
         self._seek_position(0, 0, 0, it, check_range)
         temp = np.fromfile(
-            self.fh_ac_bin, dtype=np.float64,
+            self.fh_bin, dtype=np.float64,
             count=3 * self.mx * self.my + self.mz
         )
         return temp.reshape([self.mz, self.my, self.mx, 3]).T
 
-    def read_xy(self, iz, it, check_range=True):
+    def read_field_on_xy(self, iz, it, check_range=True):
         self._seek_position(0, 0, iz, it, check_range)
         temp = np.fromfile(
-            self.fh_ac_bin, dtype=np.float64,
+            self.fh_bin, dtype=np.float64,
             count=3 * self.mx * self.my
         )
         return temp.reshape([self.my, self.mx, 3]).T
 
-    def read_x(self, iy, iz, it, check_range=True):
+    def read_field_on_x(self, iy, iz, it, check_range=True):
         self._seek_position(0, iy, iz, it, check_range)
         temp = np.fromfile(
-            self.fh_ac_bin, dtype=np.float64,
+            self.fh_bin, dtype=np.float64,
             count=3 * self.mx
         )
         return temp.reshape([self.mx, 3]).T
 
-    def read(self, ix, iy, iz, it, check_range=True):
+    def read_field(self, ix, iy, iz, it, check_range=True):
         self._seek_position(ix, iy, iz, it, check_range)
-        temp = np.fromfile(self.fh_ac_bin, dtype=np.float64, count=3)
+        temp = np.fromfile(self.fh_bin, dtype=np.float64, count=3)
         return temp.reshape([self.mx, 3]).T
 
     def __enter__(self):
---
+++ oblique.py
@@ -0,0 +1,81 @@
+import sys
+import os
+import numpy as np
+from numpy import sin, cos, exp, pi, e, sqrt
+
+import dataframe
+
+c_light = 137.036
+
+
+class ObliqueData:
+
+    def __init__(self, title, angle=0.0):
+        self.df = dataframe.DataFrame(title)
+        self.theta = angle * (pi / 180.0)
+        self._r_tx = (c_light * self.fd.dt) / (cos(self.theta) * self.fd.dx)
+        self._r_yt = (sin(self.theta) * self.fd.dy) / (c_light * self.fd.dt)
+
+    def close(self):
+        return self.df.close()
+
+    def interp_field_on_x(self, iy, iz, r_it):
+        if r_it < self.fd.mt1:
+            idx = (r_it - self.fd.mt1) * self._r_tx
+            f = self.fd.read_field_on_x(iy, iz, self.fd.mt1)
+            g = np.zeros_like(f)
+            i = np.arange(self.fd.mx, dtype=float)
+            for j in range(3):
+                g[j] = np.interp(i, i + idx, f[j])
+        else:
+            it1, it2 = int(r_it), int(r_it) + 1
+            wt1, wt2 = it2 - r_it, r_it - it1
+            f1 = self.fd.read_field_on_x(iy, iz, it1)
+            f2 = self.fd.read_field_on_x(iy, iz, it2)
+            g = f1 * wt1 + f2 * wt2
+        return g
+
+    def predict_field_on_x(self, iy, iz, it, iy_piv):
+        r_it = it + (iy_piv - iy) * self._r_yt
+        if r_it < self.fd.mt2:
+            return self.interp_field_on_x(iy_piv, iz, r_it)
+        else:
+            return np.zeros([3, self.fd.mx])
+
+    def predict_field_on_x2(self, iy, iz, it, jy1, jy2):
+        fs = np.zeros([3, self.mx])
+        for jy_piv in range(jy1, jy2):
+            fs += self.predict_field_on_x(iy, iz, it)
+        return fs * (1.0 / (jy2 - jy1))
+
+    def predict_bc(self, title, jylim=None, directory=os.cwd):
+        if jylim is None:
+            jy1 = int(self.fd.my1 * 0.75 + self.fd.my2 * 0.25)
+            jy2 = int(self.fd.my1 * 0.25 + self.fd.my2 * 0.75)
+        else:
+            jy1, jy2 = jylim
+        # Export .info file:
+        file_info = os.path.join(directory, '%s.info' % title)
+        with open(file_info, 'w') as fh_info:
+            fh_info.write('%d %d %e' % (self.fd.mx1, self.fd.mx2, self.fd.dx))
+            fh_info.write('%d %d %e' % (1, 2, 0.0))
+            fh_info.write('%d %d %e' % (1, 1, 0.0))
+            fh_info.write('%d %d %e' % (1, self.fd.mt2 + 1, self.fd.dt))
+        # Export .bin file:
+        file_bin = os.path.join(directory, '%s.bin' % title)
+        with open(file_bin, 'w') as fh_bin:
+            iz = int((self.fd.mz1 + self.fd.mz2) / 2)
+            sys.stderr.write('# Export to file=%s\n' % fh_bin.name)
+            for it in range(1, self.fd.mt2 + 1 + 1):
+                f1 = self.predict_field_on_x2(self.fd.my1, iz, it, jy1, jy2)
+                f2 = self.predict_field_on_x2(self.fd.my2, iz, it, jy1, jy2)
+                f1.tofile(fh_bin)
+                f2.tofile(fh_bin)
+                if it % 1000 == 0:
+                    sys.stderr.write('# Exported it=%d\n' % it)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        return self.close()
